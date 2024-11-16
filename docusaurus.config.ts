21aaffca9cb6389778d8a7f8577858dcc924da35
Nikul Patel <nikul@z830>
2024-11-16 09:08:13 +0000

    add google analytics plugin

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -62,6 +62,10 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-216MR2XYCH',
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
